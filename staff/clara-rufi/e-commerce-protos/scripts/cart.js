f32bf2505af9c5123da1c2fecfed523e91fbbd08
lluna <lluna@example.com>
2019-01-17 23:09:20 +0100

    v2

--- staff/clara-rufi/e-commerce-protos/scripts/cart.js
+++ staff/clara-rufi/e-commerce-protos/scripts/cart.js
@@ -1,19 +1,16 @@
 function Cart (){
-    this.products = [] 
-    /*creem llista buida de productes*/ 
+    this.products = []  /*creem llista buida de productes*/ 
 }
 
 Cart.prototype.add = function(product){  
     if (!(product instanceof Product)) throw TypeError(product + ' is not a product');
     /* prototype es para q la función se pueda utTypeErrorilizar a Cart,
     pq totes les instancies de cart quedin lligades
-    creem la funció add, pq es vagin afegint els elements
-    a Cart.*/
+    creem la funció add, pq es vagin afegint els elements a Cart.*/
     this.products.push(product)
 }
 
-/*podem buscar Cart.products i veurem array de productes
-o per producte, bra*/
+/*podem buscar Cart.products i veurem array de productes o per producte, bra*/
 
 Cart.prototype.totalPrice = function(){
     return this.products.reduce(function(previousValue, currentValue, index, array) {
@@ -21,8 +18,6 @@ Cart.prototype.totalPrice = function(){
          return previousValue + currentValue.price;
     }, 0); 
 }
-
-
 
 Cart.prototype.numberOfItems = function(){
  return this.products.length
@@ -58,9 +53,11 @@ Cart.prototype.mostExpensive = function(){
    return this.products.reduce(function(accum, product){
        return accum.price < product.price? product:accum;
    });
-});
+};
 
 
+}
+/*
 Cart.prototype.cheapest = function(){
     var cheapest = this.products.map(function(product){
         return product.price
@@ -68,17 +65,6 @@ Cart.prototype.cheapest = function(){
     }).sort(function(a, b){return b-a}).pop();
 
     return cheapest
-}
-
-
-Cart.prototype.numberOf= function(type){
-    var counter = 0;
-    for (var i=0; i<this.products.length; i++){
-        if (this.products[i] instanceof type){
-            counter++;
-        }
-    } return counter
-
 }
 
 /* recorrem array per saber num de clothes i d'electronics 
@@ -98,9 +84,6 @@ Cart.prototype.productsByPriceRange= function(){
         }
     } return btwprice
 
-}*/
-
-/*
 Cart.prototype.productsByPriceRange = function(){
     var btw = this.products.reduce(function (total, amount) {
         if (amount.price > 30 && amount.price < 120) {
@@ -110,21 +93,42 @@ Cart.prototype.productsByPriceRange = function(){
     }, []);
     return btw
 }
+
+Cart.prototype.mostExpensive = function() {
+    return this.products.reduce(function(acc, productNext){
+      if (acc.price > productNext.price){
+        return acc;
+      } else {
+        return productNext;
+    }
+}
 */
 
-
 Cart.prototype.mostExpensive = function () {
-    return this.products.reduce(function (accum, product) {
-        return accum.price < product.price ? product : accum;
+    if(this.cartList.length == 0 ) return undefined;
+
+    return this.cartList.reduce(function(accumulator, product) {
+        return accumulator.price > product.price ? accumulator : product;
     });
-};
+}
 
 Cart.prototype.cheapest = function () {
-    return this.products.reduce(function (accum, product) {
-        return accum.price > product.price ? product : accum;
-    });
-};
+    if(this.cartList.length == 0 ) return undefined;
 
+    return this.cartList.reduce(function(accumulator, product) {
+        return accumulator.price < product.price ? accumulator : product;
+    });
+}
+
+Cart.prototype.numberOf= function(type){
+    var counter = 0;
+    for (var i=0; i<this.products.length; i++){
+        if (this.products[i] instanceof type){
+            counter++;
+        }
+    } return counter
+}
+/*
 Cart.prototype.numberOf = function (type) {
     if (typeof type !== 'function') throw TypeError(type + ' is not a function');
     if (type !== Product && !(type.prototype instanceof Product)) throw TypeError(type + ' is not a product type');
@@ -133,7 +137,7 @@ Cart.prototype.numberOf = function (type) {
         return product instanceof type ? ++accum : accum;
     }, 0);
 };
-
+*/
 Cart.prototype.productsByPriceRange = function(min, max) {
     if (typeof min !== 'number') throw TypeError(min + ' is not a number');
     if (typeof max !== 'number') throw TypeError(max + ' is not a number');
@@ -142,4 +146,3 @@ Cart.prototype.productsByPriceRange = function(min, max) {
         return product.price >= min && product.price <= max;
     })
 };
-
--- staff/clara-rufi/e-commerce-protos/scripts/cart.test.js
+++ staff/clara-rufi/e-commerce-protos/scripts/cart.test.js
@@ -4,13 +4,13 @@ suite("Cart methods", function () {
 
         it("succesful add values into a list", function () {
 
-            var cart1 = new Cart()
+            var cart1 = new Cart();
             var sock = new Socks('Calvin Klein', 42, 'black', 9.99);
 
-            cart1.add(sock)
+            cart1.add(sock);
 
             expect(cart1.products[0] === sock, "result must match the expected one")
-        })
+        });
 
         it("fail adding not product values", function () {
 
@@ -25,70 +25,58 @@ suite("Cart methods", function () {
 
             expect(error, 'should have thrown an error');
             expect(error instanceof TypeError, 'should have thrown TypeError');
-
-        })
-
-
-    })
-
+        });
+    });
 
     describe("totalPrice", function () {
 
         it("succesful calculatin total prices", function(){
 
-        var cart1 = new Cart()
+        var cart1 = new Cart();
 
         var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
         var slips = new Slips('Abanderado', 42, 'purple', 13.99);
         var mobile = new Mobile('Apple', 'iPhone X', 'space-gray', 999);
-        cart1.add(bra)
-        cart1.add(slips)
-        cart1.add(mobile)
+        cart1.add(bra);
+        cart1.add(slips);
+        cart1.add(mobile);
 
-        resulttotalprice = cart1.totalPrice = function(){
-       
-       resultexpected = 1051.99
+        resulttotalprice = cart1.totalPrice()
+        resultexpected = 1051.99;
 
        expect(resulttotalprice === resultexpected, "result must match the expected one")
-        }
+    });
+        it("failing calculating total prices", function () {
 
-    })
-        it("failing calculatin total prices", function () {
-
-        var cart1 = new Cart()
+        var cart1 = new Cart();
 
         var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
         var slips = new Slips('Abanderado', 42, 'purple', 13.99);
         var mobile = new Mobile('Apple', 'iPhone X', 'space-gray', "a");
-        cart1.add(bra)
-        cart1.add(slips)
-        cart1.add(mobile)
-
-        resulttotalprice = cart1.totalPrice = function(){
+        cart1.add(bra);
+        cart1.add(slips);
+        cart1.add(mobile);
         
-        resultexpected = 1051.99
-
+        resulttotalprice = cart1.totalPrice()
+        resultexpected = 1
+        
             try {
-     
-            var cart1 = new Cart()
+   
+            var cart1 = new Cart();
             var error;
-                cart1.totalPrice(price)
+                cart1.add("paper")
             } catch (err) {
                 error = err;
             }
-
-        expect(error, 'should have thrown an error');
-        expect(error instanceof TypeError, 'should have thrown TypeError');
-
-    }
-})
-})
+        expect(resulttotalprice !== resultexpected, "result must match the expected one"); 
+        });
+    });
 
     describe("numberOfItems", function () {
 
         it("succesful calculating number of items", function(){
 
-        var cart1 = new Cart()
+        var cart1 = new Cart();
 
         var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
         var slips = new Slips('Abanderado', 42, 'purple', 13.99);
@@ -101,11 +89,11 @@ suite("Cart methods", function () {
 
         resultexpected = 3
         expect(totallength === resultexpected, "result must match the expected one")
-    })
+    });
 
-    it("failing calculating number of items", function () {
+        it("failing calculating number of items", function () {
 
-        var cart1 = new Cart()
+        var cart1 = new Cart();
 
         var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
         var slips = new Slips('Abanderado', 42, 'purple', 13.99);
@@ -116,48 +104,167 @@ suite("Cart methods", function () {
         cart1.add(mobile)
         cart1.add(mobile2)
 
-        var totallength = cart1.numberOfItems([])
+        var totallength = cart1.numberOfItems();
 
             try {
      
             var cart1 = new Cart()
             var error;
-                cart1.numberOfItems([])
+                cart1.numberOfItems()
             } catch (err) {
                 error = err;
             }
 
-        expect(error, 'should have thrown an error');
-        expect(error instanceof TypeError, 'should have thrown TypeError');
-
-    })
-
-})
+        expect(totallength !== resultexpected, "result must match the expected one")
+        });
+    });
 
     describe("mostexpensive", function () {
 
-    it("succesful showing most expensive item", function(){
+        it("succesful showing most expensive item", function(){
 
-        var cart1 = new Cart()
+            var cart1 = new Cart();
+
+            var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
+            var slips = new Slips('Abanderado', 42, 'purple', 13.99);
+            var mobile = new Mobile('Apple', 'iPhone X', 'space-gray', 999);
+            cart1.add(bra);
+            cart1.add(slips);
+            cart1.add(mobile);
+            
+         
+            var expensivest = cart1.mostExpensive();
+            var resultexpected = "Mobile";
+
+            expect(expensivest === resultexpected, "result must match the expected one")
+        
+        });
+        it("failing showing most expensive item", function(){
+
+            var cart1 = new Cart();
+
+            var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
+            var slips = new Slips('Abanderado', 42, 'purple', 13.99);
+            var mobile = new Mobile('Apple', 'iPhone X', 'space-gray', 999);
+            cart1.add(bra)
+            cart1.add(slips)
+            cart1.add(mobile)
+         
+
+            expensivest = cart1.mostExpensive()
+         
+            resultexpected = "Bra"
+
+            expect(expensivest !== resultexpected, "result must match the expected one")
+        });
+    });
+    
+        describe("cheapest", function () {
+
+        it("succesful showing most cheap item", function(){
+
+        var cart1 = new Cart();
 
         var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
         var slips = new Slips('Abanderado', 42, 'purple', 13.99);
         var mobile = new Mobile('Apple', 'iPhone X', 'space-gray', 999);
-        cart1.add(bra)
-        cart1.add(slips)
-        cart1.add(mobile)
-        console.log(cart1)
+        cart1.add(bra);
+        cart1.add(slips);
+        cart1.add(mobile);
+       
+        var cheapest = cart1.cheapest();
+        var resultexpected = "Slips";
 
-        expensivest = cart1.mostExpensive()
-    console.log(expensivest)
-        resultexpected = "Mobile"
+        expect(cheapest === resultexpected, "result must match the expected one")
+        });
 
-   expect(expensivest === resultexpected, "result must match the expected one")
-    })
+        it("failing showing most cheap item", function(){
 
-})
+        var cart1 = new Cart();
+
+        var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
+        var slips = new Slips('Abanderado', 42, 'purple', 13.99);
+        var mobile = new Mobile('Apple', 'iPhone X', 'space-gray', 999);
+        cart1.add(bra);
+        cart1.add(slips);
+        cart1.add(mobile);
+        
+        var cheapest = cart1.cheapest();
+        
+        var resultexpected = "Slips";
+
+        expect(cheapest !== resultexpected, "result must match the expected one")
+        });
+    });
+    describe("numbers of items", function () {
+
+        it('should tell number of products of Type Underwear', function() {
+
+            var cart1 = new Cart();
+
+            var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
+            var slips = new Slips('Abanderado', 42, 'purple', 13.99);
+            var mobile = new Mobile('Apple', 'iPhone X', 'space-gray', 999);
+            cart1.add(bra);
+            cart1.add(slips);
+            cart1.add(mobile);
+
+            var result = cart1.numberOf(Clothing);
+
+            var expected = 5;
+
+            expect(result === expected, 'expected and result should be the same');
+        });
+
+        it('should fail when parameter is not a function', function() {
+            
+            var cart1 = new Cart();
+
+            var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
+            var slips = new Slips('Abanderado', 42, 'purple', 13.99);
+            var mobile = new Mobile('Apple', 'iPhone X', 'space-gray', 999);
+            cart1.add(bra);
+            cart1.add(slips);
+            cart1.add(mobile);
+
+            cart1.numberOf(Clothing);
+
+            var error;
+
+            try{
+                card.numberOf("sneakers");
+            } catch(err) {
+                error = err;
+            }
+
+            expect(error, 'Should have thrown an error!');
+        });
+    });
+    describe('Testing productsByPriceRange()', function () {
+
+        it('should show product between price range', function() {
+
+            var cart1 = new Cart();
+
+            var bra = new Bra('Calvin Klein', 'push-up', 80, 'cream', 39);
+            var slips = new Slips('Abanderado', 42, 'purple', 13.99);
+            var mobile = new Mobile('Apple', 'iPhone X', 'space-gray', 999);
+            cart1.add(bra);
+            cart1.add(slips);
+            cart1.add(mobile);
+
+            cart1.numberOf(Clothing);
+
+            var result = cart1.productsByPriceRange(8,300);
+
+            var expected = [bra,slips];
+
+            expect(result[0] === expected[0], 'expected and result should be the same');
+            expect(result[1] === expected[1], 'expected and result should be the same');
+        });
+    });
+});
 
 
-})
 
 
