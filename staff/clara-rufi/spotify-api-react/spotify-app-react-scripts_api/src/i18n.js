0581017851d809ee8a830baf755daa38ae72746d
lluna <lluna@example.com>
2019-02-24 14:33:47 +0100

    spotify-app-react

---
+++ staff/clara-rufi/spotify-api-react/spotify-app-react-scripts_api/src/i18n.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const i18n = {
+    'en': {
+        title: 'Spotify-App!',
+        loginTitle: 'Login form',
+        searchTitle: 'Search artists'
+    },
+
+    'es': {
+        title: 'Spotify-App!',
+        loginTitle: 'Formulario de inicio de sesión',
+        searchTitle: 'Buscador artistas'
+    },
+
+    'ca': {
+        title: 'Spotify-App!',
+        loginTitle: 'Formulari d\'inici de sessió',
+        searchTitle: 'Search artists'
+    },
+
+    'ga': {
+        title: 'Spotify-App!',
+        loginTitle: 'Formulario de comezo da sesión',
+        searchTitle: 'Search artists'
+    },
+
+    'fr': {
+        title: 'Spotify-App!',
+        loginTitle: 'Formulaire début',
+        searchTitle: 'Search artists'
+    }
+}
+
+export default i18n
